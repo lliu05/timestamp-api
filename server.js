98f0004a7138128648076ffa10ef232702bf2b1c
Li Liu <jia988727@hotmail.com>
2016-09-03 06:37:12 +0000

    working

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-var express = require('express');
+var express = require("express");
+var moment = require("moment");
+var query = require("querystring");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/message/:time', function(req, res) {
+    //console.log("works");
+    var time = req.params.time;
+    var parseTime = parseInt(time, 10);
+    var result = {
+        "unix": "",
+        "natural": ""
+    };
+
+    //not unix format input, eg: December%2015,%202015
+    if (!parseTime) {
+        var dateReadble = query.unescape(time);
+        //convert input to unix format
+        time = moment(dateReadble).valueOf()/100;
+    }
+    
+    var date = moment.unix(time);
+    result["unix"] = time;
+    result["natural"] = date.format("MMMM D, YYYY");
+    
+    res.send(result);
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.listen(8000);
---
+++ test_server.sh
@@ -0,0 +1,2 @@
+curl localhost:8000/message/1450137600
+#curl localhost:8000/message/December%2015,%202015
