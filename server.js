35918151ceab8b11ac09cd7940681d1ee0a168ec
lliu05 <jia988727@hotmail.com>
2016-09-19 00:00:36 -0700

    jslint server.js ok

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require("express");
 var path = require("path");
 var moment = require("moment");
@@ -7,33 +9,32 @@ var app = express();
 app.use(express.static(path.join(__dirname, 'static/html')));
 app.use(express.static(path.join(__dirname, 'static/css')));
 
-app.get('/message/:time', function(req, res) {
+app.get('/message/:time', function (req, res) {
     //expect "time" to be in unix time form
-    var time = req.params.time;
-    var parseTime = parseInt(time, 10);
-    var result = {
-        "unix": "",
-        "natural": ""
-    };
+    var time = req.params.time,
+        parseTime = parseInt(time, 10),
+        dateReadble = null,
+        date = null,
+        result = {
+            "unix": "",
+            "natural": ""
+        };
 
     //not unix format input, eg: December%2015,%202015
     if (!parseTime) {
         //convert input to unix format
-        var dateReadble = query.unescape(time);
-        time = moment(dateReadble, "MMMM D, YYYY", true).valueOf()/1000;
+        dateReadble = query.unescape(time);
+        time = moment(dateReadble, "MMMM D, YYYY", true).valueOf() / 1000;
     }
-    
     //retrieve info based on unix time
-    var date = moment.unix(time);
-    result["unix"] = time.toString();
-    result["natural"] = date.format("MMMM D, YYYY");
-    
+    date = moment.unix(time);
+    result.unix = time.toString();
+    result.natural = date.format("MMMM D, YYYY");
     //check if date validate
-    if (!result["unix"] || result["natural"] == "Invalid date") {
-        result["unix"] = null;
-        result["natural"] = null;
+    if (!result.unix || result.natural === "Invalid date") {
+        result.unix = null;
+        result.natural = null;
     }
-    
     res.send(result);
 });
 
